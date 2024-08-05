d33809395a6a02ca9de40653a1316760ed9169cc
Ruviii <ruvanipradeepa@gmail.com>
2024-08-06 03:46:20 +0530

    player updated

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity = 1200.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,9 +19,26 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction -1, 0, 1
 	var direction = Input.get_axis("left", "right")
+	
+	#play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("stop")
+		else:
+			animated_sprite.play("run")
+	else :
+		animated_sprite.play("jump")		
+		
+	#flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	
+	#apply movements
 	if direction:
 		velocity.x = direction * move_speed
 	else:
