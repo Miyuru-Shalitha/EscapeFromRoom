dfd9a3fc15d70d67689ae6368bc2b76c590872fb
Miyuru-Shalitha <miyurushalitha@gmail.com>
2024-08-05 21:13:31 +0530

    Add 3 block levels

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-var move_speed = 60.0
-var jump_velocity = -250.0
+@export var move_speed = 60.0
+@export var jump_velocity = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
